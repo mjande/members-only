decb478d168f5b75ce3eccd67ba43faaf51ce47e
mattand <mjande@umich.edu>
2022-07-20 15:35:51 -0400

    Reconcile devise and turbo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "members", to: "devise/sessions#new"
+  end
+  
   devise_for :members
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts
