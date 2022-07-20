f583c3482d940f33eab18189e4f84e3aba3fcad6
mattand <mjande@umich.edu>
2022-07-20 14:25:48 -0400

    Connect members to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_181220) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_20_181936) do
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,6 +27,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_181220) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "member_id", null: false
+    t.index ["member_id"], name: "index_posts_on_member_id"
   end
 
+  add_foreign_key "posts", "members"
 end
